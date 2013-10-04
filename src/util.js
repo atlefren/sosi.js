678cdcabaadad75b9bc0801feed22fb21377b644
Atle Frenvik Sveen <atle.sveen@gmail.com>
2013-10-04 23:52:30 +0200

    merge

--- src/util.js
+++ src/util.js
@@ -64,12 +64,35 @@ var SOSI = window.SOSI || {};
         7: "EPSG:27397",
         8: "EPSG:27398",
         9: "EPSG:4273",
-        21: "EPSG:25831",
-        32: "EPSG:25832",
-        23: "EPSG:25833",
-        24: "EPSG:25834",
-        25: "EPSG:25835"
-        //TODO: fill in rest of table from spec
+        21 "EPSG:32631",
+        22 "EPSG:32632",
+        23 "EPSG:32633",
+        24 "EPSG:32634",
+        25 "EPSG:32635",
+        26 "EPSG:32636"
+        //31 UTM-Sone 31 ED50 UTM 31
+        //32 UTM-Sone 32 ED50 UTM 32
+        //33 UTM-Sone 33 ED50 UTM 33
+        //34 UTM-Sone 34 ED50 UTM 34
+        //35 UTM-Sone 35 ED50 UTM 35
+        //36 UTM-Sone 36 ED50 UTM 36
+        //41 Lokalnett, uspes.
+        //42 Lokalnett, uspes.
+        //50 ED50 Geografisk ED50 Ingen Ingen
+        //51 NGO-56A (Møre) NGO1948 Gauss-Krüger
+        //52 NGO-56B (Møre) NGO1948 Gauss-Krüger
+        //53 NGO-64A (Møre) NGO1948 Gauss-Krüger
+        //54 NGO-64B (Møre) NGO1948 Gauss-Krüger
+        //72 WGS72 Geografisk WGS72 Ingen Ingen
+        //84 WGS84 Geografisk WGS84 Ingen Ingen
+        //87 ED87 Geografisk ED87 Ingen Ingen
+        //99 Egendefinert *
+        //101 Lokalnett, Oslo
+        //102 Lokalnett, Bærum
+        //103 Lokalnett, Asker
+        //104 Lokalnett, Lillehammer
+        //105 Lokalnett,Drammen
+        //106 Lokalnett, Bergen / Askøy
     };
 
 }(SOSI));
