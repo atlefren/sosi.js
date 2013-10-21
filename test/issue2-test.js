807b50254513927b450ddc5436085ccf8c31bf52
Atle Frenvik Sveen <atle.sveen@gmail.com>
2013-10-22 00:54:31 +0200

    fix error when writing testfile2.sos to topojson (closes #14)

--- test/issue2-test.js
+++ test/issue2-test.js
@@ -23,10 +23,21 @@
 
             var flate = sosidata.features.getById(5);
             assert.equals(flate.attributes.kvalitet.målemetode, NaN);
+        },
 
+        "should be able to get KURVE 606": function () {
+            var sosidata = this.parser.parse(this.sosidata);
+            var kurve606 = sosidata.features.getById(606);
+            assert(kurve606);
+        },
 
- 
-
+        //test issue 14
+        "should be able to write to TopoJSON": function () {
+            var sosidata = this.parser.parse(this.sosidata);
+            var name = "testdata";
+            var json =  sosidata.dumps("topojson", name);
+            assert(json);
         }
+
     });
 }(SOSI));
