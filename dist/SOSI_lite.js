f45b64d916bfb84f35bfe35def4a557ea4a45d29
Thomas Hirsch <thomas.hirsch@statkart.no>
2013-10-22 08:54:38 +0200

    build

--- dist/SOSI_lite.js
+++ dist/SOSI_lite.js
@@ -1,3 +1,10 @@
+if (!(typeof require=="undefined")) { /* we are running inside node.js */
+  var _ = require("underscore");
+  var proj4 = require("proj4");
+  var window = window || {};
+  window.SOSI = window.SOSI || {};
+}
+
 var SOSI = window.SOSI || {};
 
 /**
@@ -972,6 +979,38 @@ var SOSI = window.SOSI || {};
     ns.Parser = ns.Base.extend({
         parse: function (data) {
             return new SosiData(ns.util.parseTree(splitOnNewline(data), 1));
+        },
+        getFormats: function() {
+            return _.keys(dumpTypes);
         }
     });
 }(SOSI));
+
+if (!(typeof require == "undefined")) { /* we are running inside nodejs */
+  var fs = require("fs")
+  var util = require("util")
+
+  var parser = new SOSI.Parser();
+  
+  if (process.argv.length < 4) { 
+    util.print("\nusage: nodejs SOSI.js.js format infile.sos > outfile\n\n"
+               + "where: format    : one of [" + parser.getFormats() + "]\n"
+               + "       infile.sos: a file in SOSI format\n"
+               + "       outfile   : an output file name, omit for stdout\n\n"
+    );
+    process.exit(1);
+  } 
+
+  var format   = process.argv[2],
+      filename = process.argv[3];
+  fs.readFile(filename, "utf8", function(err, data) {
+    /* todo: detect TEGNSETT/encoding in the first bytes and re-read the file with the proper encoding */
+    if (err) {
+      return util.print(err);
+    }
+    json       = parser.parse(data).dumps(format);
+    util.print(JSON.stringify(json)); /* only for GeoJSON or TopoJSON */
+  });
+
+}
+
